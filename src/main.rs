7618fc423c8b51b95cc0cdd668beda4963833f6c
Fabrice Sznajderman <fabszn@protonmail.com>
2023-08-16 09:45:57 +0200

    step1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-    use std::collections::HashMap;
+use std::fs;
+use json::parse;
+use std::collections::HashMap;
 
 
-    #[tokio::main]
-    async fn main() -> Result<(), Box<dyn std::error::Error>> {
-        let resp = reqwest::get("https://httpbin.org/ip")
-            .await?
-            .json::<HashMap<String, String>>()
-            .await?;
-        println!("{:#?}", resp);
-        Ok(())
-    }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+
+fn parse_json() {
+    let json = fs::read_to_string("./yt.json").expect("Error occured while reading fil");
+
+    let data = parse(&json).expect("parser error");
+
+    let count = &data["items"][0];
+
+    println!("{}", count);
+    println!("Hello, world!");
+}
+
