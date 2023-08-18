b97f4e35ffc24696d4ea02c0a35972cfc7a958d5
Fabrice Sznajderman <fabrice.sznajderman-ext@mnt.fr>
2023-08-18 17:07:58 +0200

    merge

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,24 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    //AIzaSyDuRlgSAqroomabL3bMGMdzd-aRdKIV9TM
+
+    
+
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
+
     println!("{:#?}", resp);
+    parse_json();
     Ok(())
 }
 
 
 fn parse_json() {
+    println!("{:#?}","parseJdon");
     let json = fs::read_to_string("./yt.json").expect("Error occured while reading fil");
 
     let data = parse(&json).expect("parser error");
