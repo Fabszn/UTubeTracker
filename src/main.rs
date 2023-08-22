0aa9d8792c5ae737d4d10cbd53b43ed0187cd082
Fabrice Sznajderman <fabszn@protonmail.com>
2023-08-22 20:36:09 +0200

    close to query youtube

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 use std::fs;
 use json::parse;
 use std::collections::HashMap;
-
+use reqwest;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result = youtube_request();
+    match result.await {
+        Ok(value) => println!("Result: {}", value),
+        Err(err) => println!("Error: {}", err),
+    }
 
-    //AIzaSyDuRlgSAqroomabL3bMGMdzd-aRdKIV9TM
 
-    
 
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-
-    println!("{:#?}", resp);
-    parse_json();
     Ok(())
 }
 
 
+async fn youtube_request() -> Result<HashMap<String, String>, reqwest::Error> {
+    let t = reqwest::get(
+        "")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    Ok(t)
+}
+
 fn parse_json() {
-    println!("{:#?}","parseJdon");
     let json = fs::read_to_string("./yt.json").expect("Error occured while reading fil");
 
     let data = parse(&json).expect("parser error");
