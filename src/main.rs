bdd1b60cb89f9e1014840b89156a8a80f9343e66
Fabrice Sznajderman <fabszn@protonmail.com>
2023-09-26 10:02:05 +0200

    step 2

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,60 @@
-use std::fs;
 use json::parse;
-use std::collections::HashMap;
 use reqwest;
+use std::fs;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let result = youtube_request();
+   youtube_key =  match env::var("YOUTUBE_API_KEY") {
+        Ok(value) => {
+            println!("The value of MY_ENV_VARIABLE is: {}", value);
+            value;
+        }
+        Err(_) => {
+            println!("MY_ENV_VARIABLE is not set.");
+
+        }
+    }
+
+    let result = nb_video_request();
     match result.await {
         Ok(value) => println!("Result: {}", value),
         Err(err) => println!("Error: {}", err),
     }
 
-
-
     Ok(())
 }
 
 
-async fn youtube_request() -> Result<i16, reqwest::Error> {
-    let reponse = reqwest::get("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCpH-VFo4_F2oMYiW8942mQw&key=")
+async fn nb_video_request(youtube_key: String) -> Result<String, reqwest::Error> {
+    let reponse: String = reqwest::get("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCpH-VFo4_F2oMYiW8942mQw&key=")
+
         .await?
-        .json()
+        .text()
         .await?;
 
     let data = parse(&reponse).expect("parsing error");
-    let nbVideo = &data["items"][0]["statistics"]["videoCount"].as_i16().expect("convert to i16 error");
+    let nb_video = &data["items"][0]["statistics"]["videoCount"];
 
-    Ok(*nbVideo)
+    Ok(nb_video.to_string())
 }
 
-fn parse_json() {
+async fn video_by_channel_id_request(
+    playlistId: String,
+    youtube_key: String,
+) -> Result<String, reqwest::Error> {
+    let reponse: String = reqwest::get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=UUpH-VFo4_F2oMYiW8942mQw&key=")
+        .await?
+        .text()
+        .await?;
+
+    let data = parse(&reponse).expect("parsing error");
+    let nb_video = &data["items"][0]["statistics"]["videoCount"];
+
+    Ok(nb_video.to_string())
+}
+
+/*fn parse_json() {
     let json = fs::read_to_string("./yt.json").expect("Error occured while reading fil");
 
     let data = parse(&json).expect("parser error");
@@ -38,5 +63,4 @@ fn parse_json() {
 
     println!("{}", count);
     println!("Hello, world!");
-}
-
+}*/
