c4195d978cf723c2f8a762d1ce663470f521bcd6
Fabrice Sznajderman <fabszn@protonmail.com>
2023-09-29 09:57:35 +0200

    step 3

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use json::parse;
 use reqwest;
-use std::fs;
-use std::env;
+use std::env
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-   youtube_key =  match env::var("YOUTUBE_API_KEY") {
+
+   let youtube_key =  match env::var("YOUTUBE_API_KEY") {
         Ok(value) => {
             println!("The value of MY_ENV_VARIABLE is: {}", value);
-            value;
+            &value;
         }
         Err(_) => {
             println!("MY_ENV_VARIABLE is not set.");
-
+            "No key found"        
         }
-    }
+    };
 
-    let result = nb_video_request();
+
+    let result = video_by_channel_id_request("UUpH-VFo4_F2oMYiW8942mQw",youtube_key);
     match result.await {
         Ok(value) => println!("Result: {}", value),
         Err(err) => println!("Error: {}", err),
@@ -40,7 +42,7 @@ async fn nb_video_request(youtube_key: String) -> Result<String, reqwest::Error>
 }
 
 async fn video_by_channel_id_request(
-    playlistId: String,
+    playlistId: &str,
     youtube_key: String,
 ) -> Result<String, reqwest::Error> {
     let reponse: String = reqwest::get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=UUpH-VFo4_F2oMYiW8942mQw&key=")
@@ -49,7 +51,7 @@ async fn video_by_channel_id_request(
         .await?;
 
     let data = parse(&reponse).expect("parsing error");
-    let nb_video = &data["items"][0]["statistics"]["videoCount"];
+    let nb_video = &data["items"]["resourceId"]["videoId"];
 
     Ok(nb_video.to_string())
 }
