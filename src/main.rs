96a3757b9c3d5ed50802f726e414777c209fcee1
Fabrice Sznajderman <fabszn@protonmail.com>
2023-10-04 09:56:53 +0200

    get video list

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 use json::parse;
 use reqwest;
-use std::env
+use std::env;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+   
 
-   let youtube_key =  match env::var("YOUTUBE_API_KEY") {
+    let check = "toto";
+    let test = match check {
+        "toto" => "GO",
+        "titi" => "NO GO",
+        _ => "OUT",
+    };
+    println!("{}", test);
+
+
+   let env_ytube_key = env::var("YOUTUBE_API_KEY");
+   
+   let youtube_key =  match env_ytube_key  {
         Ok(value) => {
-            println!("The value of MY_ENV_VARIABLE is: {}", value);
-            &value;
-        }
+            println!("env var : YOUTUBE_API_KEY > {}",value );
+            value
+        },
         Err(_) => {
-            println!("MY_ENV_VARIABLE is not set.");
-            "No key found"        
-        }
+            println!("YOUTUBE_API_KEY is not set.");
+            "No key found".to_string()
+        },
     };
 
 
-    let result = video_by_channel_id_request("UUpH-VFo4_F2oMYiW8942mQw",youtube_key);
+    let result = video_by_channel_id_request("UUpH-VFo4_F2oMYiW8942mQw",&youtube_key);
     match result.await {
         Ok(value) => println!("Result: {}", value),
         Err(err) => println!("Error: {}", err),
-    }
+    };
 
     Ok(())
 }
 
 
-async fn nb_video_request(youtube_key: String) -> Result<String, reqwest::Error> {
-    let reponse: String = reqwest::get("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCpH-VFo4_F2oMYiW8942mQw&key=")
+async fn nb_video_request(playlist_id: &str,youtube_key: &str) -> Result<String, reqwest::Error> {
+    let reponse: String = reqwest::get(format!("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id={}&key={}",playlist_id,youtube_key))
 
         .await?
         .text()
@@ -42,15 +54,21 @@ async fn nb_video_request(youtube_key: String) -> Result<String, reqwest::Error>
 }
 
 async fn video_by_channel_id_request(
-    playlistId: &str,
-    youtube_key: String,
+    playlist_id: &str,
+    youtube_key: &str,
 ) -> Result<String, reqwest::Error> {
-    let reponse: String = reqwest::get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=UUpH-VFo4_F2oMYiW8942mQw&key=")
+    
+
+    let formatted_url = format!("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId={}&key={}",   playlist_id,youtube_key);
+    
+    println!("{}", formatted_url);
+    let reponse: String = reqwest::get(formatted_url)
         .await?
         .text()
         .await?;
 
     let data = parse(&reponse).expect("parsing error");
+    println!("data from request : {}",&data);
     let nb_video = &data["items"]["resourceId"]["videoId"];
 
     Ok(nb_video.to_string())
